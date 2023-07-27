22f16db64d994e72a3048a69a9200d746aa1d8c5
abhishek <abk1379gupta@gmail.com>
2023-07-27 16:34:12 +0530

    shareReplay

--- src/app/rpraco/rpraco.component.ts
+++ src/app/rpraco/rpraco.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { every, from, of, sequenceEqual, switchMap, timer } from 'rxjs';
+import { every, from, of, sequenceEqual, shareReplay, switchMap, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-rpraco',
@@ -23,6 +23,29 @@ const expseq = from([4,5,6])
      of([1,2,3], [4,5,6], [7,8,9]).pipe(
       switchMap(arr => from(arr).pipe(sequenceEqual(expseq)))
      ).subscribe(console.log)
-   }
 
+
+
+     const source$ = of(1, 2, 3);
+
+     let data = source$.pipe(
+       tap((i) => console.log(`Received ${i}`)),
+       shareReplay(1) //bufferSize
+     );
+     
+     data.subscribe((val: any) => {
+       console.log(val);
+     });
+     
+     data.subscribe((val: any) => {
+       console.log(val);
+     });
+     
+     data.subscribe((val: any) => {
+       console.log(val);
+     });
+     
+   }
+  
+   
 }
